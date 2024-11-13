ae8228ceb99abe978800703818f35e8ebd6338fc
amanyadav0895 <yadavaman0895@gmail.com>
2024-11-13 14:29:50 +0530

    Add files via upload

--- aman.c
+++ aman.c
@@ -1,17 +1,56 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    char c;
-
-    printf("Enter an c: ");
-    scanf("%c", &c);
-
-    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
-        printf("%c is a vowel.\n", c);
-    } else {
-        printf("%c is a consonant.\n", c);
+int main()
+{
+    int a[100], b[100], m, n, i, j, k, merge[200];
+    printf("Enter size of first array: ");
+    scanf("%d", &m);
+    printf("Enter elements of first array: ");
+    for(i=0; i<m; i++)
+    {
+        scanf("%d", &a[i]);
     }
-
+    printf("Enter size of second array: ");
+    scanf("%d", &n);
+    printf("Enter elements of second array: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    i=0;
+    j=0;
+    k=0;
+    while(i<m && j<n)
+    {
+        if(a[i]<b[j])
+        {
+            merge[k]=a[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            merge[k]=b[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<m)
+    {
+        merge[k]=a[i];
+        i++;
+        k++;
+    }
+    while(j<n)
+    {
+        merge[k]=b[j];
+        j++;
+        k++;
+    }
+    printf("Merged array is: ");
+    for(i=0; i<m+n; i++)
+    {
+        printf("%d ", merge[i]);
+    }
     return 0;
 }
